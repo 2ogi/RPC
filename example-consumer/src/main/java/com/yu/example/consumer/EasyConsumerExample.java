660219bd286e26d53bc1db337322eb645c00cfc1
2ogi <2422972116@qq.com>
2024-03-06 19:49:58 +0800

    实现 静态/动态代理

--- example-consumer/src/main/java/com/yu/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/yu/example/consumer/EasyConsumerExample.java
@@ -2,6 +2,7 @@ package com.yu.example.consumer;
 
 import com.yu.example.common.model.User;
 import com.yu.example.common.service.UserService;
+import com.yu.rpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
@@ -9,8 +10,10 @@ import com.yu.example.common.service.UserService;
 public class EasyConsumerExample {
 
     public static void main(String[] args){
-        // todo 需要获取UserService实现类对象
-        UserService userService = null;
+//        // 静态代理
+//        UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("2ogi");
         // 调用
