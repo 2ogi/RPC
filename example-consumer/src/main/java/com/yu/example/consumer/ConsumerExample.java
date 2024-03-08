0c6be981190df052ef58cc5596a726ec33a4060c
2ogi <2422972116@qq.com>
2024-03-08 15:44:35 +0800

    增加 mock服务

--- example-consumer/src/main/java/com/yu/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yu/example/consumer/ConsumerExample.java
@@ -1,12 +1,28 @@
 package com.yu.example.consumer;
 
+import com.yu.example.common.model.User;
+import com.yu.example.common.service.UserService;
 import com.yu.rpc.config.RpcConfig;
+import com.yu.rpc.proxy.ServiceProxyFactory;
 import com.yu.rpc.utils.ConfigUtils;
 
 public class ConsumerExample {
 
     public static void main(String[] args) {
-        RpcConfig rpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpcConfig);
+//        RpcConfig rpcConfig = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+//        System.out.println(rpcConfig);
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getMockProxy(UserService.class);
+        User user = new User();
+        user.setName("lin");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null){
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
